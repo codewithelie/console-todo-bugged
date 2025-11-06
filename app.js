63a196c9cc62ac20fe3ae73686f516ac08e4e842
codewithelie <me@elyh.fr>
2025-11-06 13:01:31 +0100

    feat: gestion basique du fichier todos.json

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Bienvenue dans TodoConsole !");
+const fs = require('fs');
+const path = require('path');
+
+const FILE = path.join(__dirname, 'todos.json');
+
+if (!fs.existsSync(FILE)) fs.writeFileSync(FILE, JSON.stringify([]));
+
+function readTodos() {
+  const data = fs.readFileSync(FILE, 'utf8');
+  return data ? JSON.parse(data) : [];
+}
+
+function writeTodos(todos) {
+  fs.writeFileSync(FILE, JSON.stringify(todos, null, 2));
+}
+
+console.log("📋 TodoConsole prêt !");
